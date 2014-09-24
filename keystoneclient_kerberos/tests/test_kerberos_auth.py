e95f28744b99981564e468a1c997af8e503727f3
Jose Castro Leon <jose.castro.leon@cern.ch>
2014-09-23 20:20:25 -0400

    Initial kerberos plugin implementation.
    Enable kerberos when doing authentication.
    It will reuse any existing kerberos token and use it if the server
    uses WWW-Authenticate: Negotiate.
    depends on to requests-kerberos
    
    Co-Authored by Adam Young <ayoung@redhat.com>
    
    bp: kerberos-auth-plugin
    
    Change-Id: Idf02bf27b5933c00827dd08d11ac131896184ad8

---
+++ keystoneclient_kerberos/tests/test_kerberos_auth.py
@@ -0,0 +1,50 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+from keystoneclient import session
+from keystoneclient.tests import utils
+import mock
+import requests_kerberos
+
+from keystoneclient_kerberos import kerberos
+
+
+class TestKerberosAuth(utils.V3AuthTestCase):
+
+    @mock.patch.object(requests_kerberos.HTTPKerberosAuth,
+                       'generate_request_header')
+    def test_authenticate_with_kerberos_domain_scoped(self, request_header):
+        header = 'Negotiate %s' % uuid.uuid4().hex
+        request_header.return_value = header
+
+        fail_resp = {'text': 'Fail',
+                     'status_code': 401,
+                     'headers': {'WWW-Authenticate': 'Negotiate'}}
+        pass_resp = {'json': self.TEST_RESPONSE_DICT,
+                     'status_code': 200,
+                     'headers': {'X-Subject-Token': self.TEST_TOKEN}}
+
+        self.stub_url(method='POST', parts=['auth', 'tokens'],
+                      response_list=[fail_resp, pass_resp])
+
+        a = kerberos.Kerberos(self.TEST_URL)
+        s = session.Session(a)
+        s.get_token()
+
+        req = {'auth': {'identity': {'methods': ['kerberos'],
+                                     'kerberos': {}}}}
+
+        self.assertRequestBodyIs(json=req)
+        self.assertRequestHeaderEqual('Authorization', header)
+        self.assertEqual(s.auth.auth_ref.auth_token, self.TEST_TOKEN)
